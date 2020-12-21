dbbbc42b2d758bc25e9a991394d3f9a534e6cd07
Alicia Chumchai Toom <alicia.chumchai@gmail.com>
2020-12-21 11:40:52 +0100

    edit code

--- App/Controller/import_to_db_from_json.py
+++ App/Controller/import_to_db_from_json.py
@@ -4,10 +4,18 @@ import os
 
 
 def import_jsons_as_books(path):
+    # default_cover = import_default_book_cover(path)
     files = os.listdir(path)
     for file in files:
-        if file[-5:] == '.json':
+        if file.endswith(".json"):
+        #if file[-5:] == '.json':
             with open(path + file, encoding='utf-8') as f:
                 json_data = json.load(f)
+                # json_data['cover'] = default_cover
                 new_book = Book(json_data)
                 new_book.save()
+
+
+# def import_default_book_cover(path):
+#     with open(path + 'default_cover.jpg', 'rb') as f:
+#         return bytes(f.read())
