e83f0a0217c3e85506041044a9d48bdf8a5b75cb
Alicia Chumchai Toom <alicia.chumchai@gmail.com>
2020-12-22 17:26:51 +0100

    Test import book cover from open library api and save to mongo db

--- App/Controller/import_to_db_from_json.py
+++ App/Controller/import_to_db_from_json.py
@@ -1,32 +1,54 @@
+from urllib.error import HTTPError
+
 from Model.MongoDB.Models.authors import Author
 from Model.MongoDB.Models.books import Book
 import json
 import os
+import urllib.request
+from bson.binary import Binary
 
 
 def import_jsons_as_books(path):
-    # default_cover = import_default_book_cover(path)
     files = os.listdir(path)
     for file in files:
         if file.endswith(".json"):
-        #if file[-5:] == '.json':
-            with open(path + file, encoding='utf-8') as f:
+            with open(os.path.join(path, file), encoding='utf-8') as f:
                 json_data = json.load(f)
-                # json_data['cover'] = default_cover
+
+                cover_file = os.path.join(path, file.replace(".json", ".jpg"))
+                if os.path.exists(cover_file):
+                    with open(cover_file, "rb") as c:
+                        cover_image = Binary(c.read())
+                        json_data['cover_image'] = cover_image
+                else:
+                    # TODO Load default cover image
+                    pass
+
                 new_book = Book(json_data)
                 new_book.save()
 
+def download_book_covers(path):
+    files = os.listdir(path)
+    for file in files:
+        if file.endswith(".json"):
+            with open(os.path.join(path, file), encoding='utf-8') as f:
+                json_data = json.load(f)
+                cover_url = f"http://covers.openlibrary.org/b/isbn/{json_data['ISBN']}-M.jpg?default=false"
+                cover_file = os.path.join(path, file.replace(".json", ".jpg"))
 
-# def import_default_book_cover(path):
-#     with open(path + 'default_cover.jpg', 'rb') as f:
-#         return bytes(f.read())
+                try:
+                    urllib.request.urlretrieve(cover_url, cover_file)
+                    print(f"Successfully downloaded cover for url: {cover_url} to file: {cover_file}")
+                except HTTPError:
+                    print(f"Unable to download cover from url: {cover_url} to file: {cover_file}")
+
 
 def import_jsons_as_authors(path):
     files = os.listdir(path)
     for file in files:
         print(file)
         if file.endswith(".json"):
-            with open(path + file, encoding='utf-8') as f:
+            with open(os.path.join(path, file), encoding='utf-8') as f:
                 json_data = json.load(f)
                 new_author = Author(json_data)
                 new_author.save()
--- App/main.py
+++ App/main.py
@@ -1,4 +1,5 @@
-from Controller.import_to_db_from_json import import_jsons_as_books, import_jsons_as_authors
+from Controller.import_to_db_from_json import import_jsons_as_books, import_jsons_as_authors, \
+    download_book_covers
 
 
 def main():
@@ -6,6 +7,7 @@ def main():
     #import_jsons_as_authors(folder_path)
 
     folder_path = 'Model/MongoDB/Models/books_db/'
+    #download_book_covers(folder_path)
     import_jsons_as_books(folder_path)
 
 if __name__ == '__main__':
