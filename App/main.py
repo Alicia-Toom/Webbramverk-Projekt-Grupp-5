e860d8d0d45ba5d422fdd0c5b1d2b0ef8d03a9df
Oscar Widing <widing.oscar@gmail.com>
2020-12-22 13:46:00 +0100

    change to structure, and base html

--- App/main.py
+++ App/main.py
@@ -3,7 +3,7 @@ from Controller.import_to_db_from_json import import_jsons_as_books, import_json
 
 def main():
     folder_path = 'Model/MongoDB/Models/authors_db/'
-    import_jsons_as_authors(folder_path)
+    #import_jsons_as_authors(folder_path)
 
 
 if __name__ == '__main__':
