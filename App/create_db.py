eed23f336632653ec014f56dd26be7b2f53c9357
Oscar Widing <widing.oscar@gmail.com>
2021-01-15 13:52:54 +0100

    added user class in createdb

--- App/create_db.py
+++ App/create_db.py
@@ -1,5 +1,18 @@
+from Model.MySQL.DB import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=True)
+    last_name = db.Column(db.String(100), nullable=True)
+    email = db.Column(db.String(100), unique=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.VARCHAR(255))
+
+
 def main():
-    pass
+
+    db.create_all()
 
 
 if __name__ == "__main__":
