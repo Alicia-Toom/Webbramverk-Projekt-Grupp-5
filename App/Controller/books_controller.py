a694634647b6846c3f7e86a5bc30322849184e80
Alicia Chumchai Toom <alicia.chumchai@gmail.com>
2021-01-15 16:27:25 +0100

    Author page extended from base.html

--- App/Controller/books_controller.py
+++ App/Controller/books_controller.py
@@ -28,13 +28,11 @@ def book(book_id):
 
 @books.route('/books/<book_id>/cover')
 def cover(book_id):
-    # Example on how to use in HTML: <img src="/books/{{ book._id }}/cover" />
     book = Book.find(_id=ObjectId(book_id)).first_or_none()
     if book is not None and book.cover_image is not None:
         cover_image = book.cover_image
         response = make_response(cover_image)
         response.headers.set('Content-Type', 'image/jpeg')
         return response
-    else: #remove the american gods
-        # TODO Return default cover
-        abort(404) #http respornse code 404=notfound
+    else:
+        abort(404)
