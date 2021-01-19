79a025c8b325b5876c8627af7f5ba6d56d512286
Oscar Widing <widing.oscar@gmail.com>
2021-01-19 14:40:19 +0100

    checking if user already exists

--- App/Controller/sign_up_controller.py
+++ App/Controller/sign_up_controller.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, request, redirect, url_for
+from flask import render_template, Blueprint, request, redirect, url_for, session
 import bcrypt
 signups = Blueprint('signups', __name__)
 from Model.MySQL.Models.users import User
@@ -17,8 +17,10 @@ def signup_post():
     password = request.form['password']
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(str.encode(password), salt)
-    user = User(username=username, email=email, password=hashed_password, first_name="oscar", last_name="widing")
-    print()
-
-    users_repo.add_user(user)
-    return redirect(url_for('log_in.html'))
+    if User.query.filter_by(username=username).first():
+        return render_template('sign_up.html', taken=True)
+    else:
+        user = User(username=username, email=email, password=hashed_password, first_name="oscar", last_name="widing")
+        users_repo.add_user(user)
+        session['username'] = user.username
+        return redirect(url_for('index'))
--- App/Model/MySQL/Repository/users_repo.py
+++ App/Model/MySQL/Repository/users_repo.py
@@ -1,6 +1,10 @@
 from App.Viewer.app import db
 
 
+def find_username(user):
+    return user.query.filter_by(username=user.username).first()
+
+
 def add_user(user):
     db.session.add(user)
     db.session.commit()
