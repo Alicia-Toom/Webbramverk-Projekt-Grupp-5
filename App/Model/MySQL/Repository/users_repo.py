9af0e52d5eedded8089e8fea7936044f01adc7ef
Oscar Widing <widing.oscar@gmail.com>
2021-01-25 09:33:23 +0100

    making of profile page

--- App/Model/MySQL/Repository/users_repo.py
+++ App/Model/MySQL/Repository/users_repo.py
@@ -6,5 +6,6 @@ def add_user(user):
     db.session.add(user)
     db.session.commit()
 
+
 def find_by_username(username):
     return db.session.query(User).filter(User.username == username).first()
