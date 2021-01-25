7b02e4b6558b79e657a3f9d001b99ade61663b87
Martin-E-Karlsson <m.karlsson91@gmail.com>
2021-01-25 17:27:01 +0100

    Relocated and renamed App/main.py to App/Model/MongoDB/DB/import_to_mongodb.py

--- App/Model/MongoDB/DB/import_to_mongodb.py
+++ App/Model/MongoDB/DB/import_to_mongodb.py
@@ -2,16 +2,16 @@ from Controller.Utils.import_to_mongodb import import_jsons_as_books, \
     import_jsons_as_authors
 
 
-def main():
-    folder_path = 'Model/MongoDB/Models/authors_db/'
+def import_to_mongodb():
+    folder_path = '../Models/authors_db/'
     # download_author_photo(folder_path)
     import_jsons_as_authors(folder_path)
 
 
-    folder_path = 'Model/MongoDB/Models/books_db/'
+    folder_path = '../Models/books_db/'
     # download_book_covers(folder_path)
     import_jsons_as_books(folder_path)
 
 
 if __name__ == '__main__':
-    main()
+    import_to_mongodb()
