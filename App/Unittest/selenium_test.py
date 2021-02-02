ba8852cda86792183b280ed864cd02eadc78775f
Oscar Widing <widing.oscar@gmail.com>
2021-02-02 15:23:31 +0100

    selenium signup, working on making unittest to see if it can create and delete user

--- App/Unittest/selenium_test.py
+++ App/Unittest/selenium_test.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+from Model.MySQL.Models.users import User
+from Model.MySQL.Repository.users_repo import find_all
+
 
 def login(driver):
     driver.get('http://127.0.0.1:5000/login')
@@ -23,10 +26,30 @@ def change_name(driver):
     submit.send_keys(Keys.RETURN)
 
 
+def sign_up(driver):
+    password = "thisissecret"
+    username = "selenium"
+    email = "email@email.com"
+    driver.get('http://127.0.0.1:5000/login/signup')
+    username_field = driver.find_element_by_name('username')
+    email_field = driver.find_element_by_name('email')
+    password_field = driver.find_element_by_name('password')
+    confirm_field = driver.find_element_by_name('confirm_password')
+    submit = driver.find_element_by_id('submit')
+    users = find_all()
+    username_field.send_keys(username)
+    email_field.send_keys(email)
+    password_field.send_keys(password)
+    confirm_field.send_keys(password)
+    #submit.send_keys(Keys.RETURN)
+
+
+
 def main():
     driver = webdriver.Chrome('chromedriver.exe')
-    login(driver)
-    change_name(driver)
+    #login(driver)
+    #change_name(driver)
+    sign_up(driver)
     time.sleep(5)
     driver.close()
 
