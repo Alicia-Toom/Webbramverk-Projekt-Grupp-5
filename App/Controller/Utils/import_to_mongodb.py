b2dabf4f8ecb706ac2b215b720318df3e0b523b7
Martin-E-Karlsson <m.karlsson91@gmail.com>
2021-02-09 11:04:36 +0100

    Fixed leftover issues with index page from last pull request merge

--- App/Controller/Utils/import_to_mongodb.py
+++ App/Controller/Utils/import_to_mongodb.py
@@ -92,22 +92,22 @@ def main():
 
 
     folder_path = '../../Model/MongoDB/Models/books_db/'
-    #download_book_covers(folder_path)
-    #import_jsons_as_books(folder_path)
+    download_book_covers(folder_path)
+    import_jsons_as_books(folder_path)
 
 
     folder_path = '../../Model/MongoDB/Models/books_db/best_of_the_year'
-    #download_book_covers(folder_path)
-    #import_jsons_as_books(folder_path)
+    download_book_covers(folder_path)
+    import_jsons_as_books(folder_path)
 
     folder_path = '../../Model/MongoDB/Models/books_db/new_titles'
-    # download_book_covers(folder_path)
-    #import_jsons_as_books(folder_path)
+    download_book_covers(folder_path)
+    import_jsons_as_books(folder_path)
 
 
     folder_path = '../../Model/MongoDB/Models/books_db/hot_titles'
-    #download_book_covers(folder_path)
-    #import_jsons_as_books(folder_path)
+    download_book_covers(folder_path)
+    import_jsons_as_books(folder_path)
 
 
 
